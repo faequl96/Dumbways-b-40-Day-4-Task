2c62a83db7faa780c99fde52f5ae99476347ec0e
Faeq <faequl96@gmail.com>
2022-09-16 08:25:19 +0700

    memperbaiki bug pada script dan memperbaiki visual

--- tangkapData.js
+++ tangkapData.js
@@ -16,31 +16,37 @@ function tangkapData() {
         if(obj.node == 'nodeJs') {
             obj.nodeJs = 'img/node.png'
         }
+    }  else {
+        obj.nodeJs = ""
     }
     if(document.querySelector('.reactJs').checked) {
         obj.react = document.querySelector('.reactJs').value;
         if(obj.react == 'reactJs') {
             obj.reactJs = 'img/react.png'
         }
+    } else {
+        obj.reactJs = ""
     }
     if(document.querySelector('.vueJs').checked) {
         obj.vue = document.querySelector('.vueJs').value;
         if(obj.vue == 'vueJs') {
             obj.vueJs = 'img/vue.png'
         }
+    } else {
+        obj.vueJs = ""
     }
     if(document.querySelector('.typeScript').checked) {
         obj.typeScript = document.querySelector('.typeScript').value;
         if(obj.typeScript == 'typeScript') {
-            obj.typeScriptJs = 'img/typescript.png'
+            obj.typeScript = 'img/typescript.png'
         }
+    } else {
+        obj.typeScript = ""
     }
 
 
     dataForm.push(obj);
     console.log(dataForm);
-
-    console.log(dataForm[0].uploadImg)
 
     for(let i = 0; i < dataForm.length; i++) {
         if(i == dataForm.length-1) {
@@ -51,9 +57,19 @@ function tangkapData() {
                         <img src="${dataForm[i].uploadImg}" alt="" class="imgContent">
                     </div>
                     <div class="judulCard">
-                        <h3>${dataForm[i].projectName}</h3>
+                        <div class="kotakJudul">
+                            <h3>${dataForm[i].projectName}</h3>
+                        </div>
                         <div class="durasi">
-                            <p>Durasi:</p> <p>${dataForm[i].startDate} - ${dataForm[i].endDate}</p>
+                            <div>
+                                <p>Start</p>
+                                <p>End</p>
+                            </div>
+                            <div>
+                                <p>: ${dataForm[i].startDate}</p>
+                                <p>: ${dataForm[i].endDate}</p>
+                            </div>
+                            
                         </div>
                         
                     </div>
